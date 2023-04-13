7422366cf1fa25815fea4a55325a8f79132c7df3
MaharramAhmadli <127186172+MaharramAhmadli@users.noreply.github.com>
2023-04-13 09:49:34 +0400

    Add files via upload

---
+++ JS task 1/assets/js/main.js
@@ -0,0 +1,33 @@
+// verilmis 3 reqemli ededin butun reqemlerininbir birinden ferqli olub olmadigini tapan alqoritm
+
+
+// var a=457;
+// var teklik=a%10;
+// console.log(teklik);
+// var onluq=(a-teklik)%100/10;
+// console.log(onluq);
+// var yuzluk=(a-a%100)%1000/100;
+// console.log(yuzluk);
+// if(teklik==onluq&&teklik==yuzluk&&yuzluk==onluq){
+//     console.log("beraberdir");
+// }
+// else{
+//     console.log("beraber deyil");
+// }
+
+
+
+alert()
+var href=prompt("herf daxil edin");
+switch(href){
+    case "a":
+    console.log("Azerbaycan dili secildi");
+    break;
+    case "r":
+        console.log("Rus dili secildi");
+        break;
+        default:
+            console.log("sehvdir");
+            break;
+
+}
